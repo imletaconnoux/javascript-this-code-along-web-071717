2ede57fff7c65b5d031bd91db9ac3442ecc9d934
imletaconnoux <imletaconnoux@gmail.com>
2017-08-30 17:49:30 -0400

    coded along

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+
+
+
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+}
+
+function serve() {
+    if(arguments.length > 0) {
+        var customers = Array.prototype.slice.call(arguments);
+        last = customers.pop();
+        console.log(this.name + " for " + customers.join(", ") + " and " + last + ". Enjoy!");
+    }else {
+        console.log(this.name + ". Order up!");
+    }
+}
+
+var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"], "Peanut Butter & Jelly");
